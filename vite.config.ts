7087deec0602269838a8789309b90912d52e1b0b
Tushar Kanti Mahato <tushargkd230@gmail.com>
2026-08-20 00:24:06 +0530

    Fix GitHub Pages asset deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Predict-Diabetes-with-Machine-Learning-/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
